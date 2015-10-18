07ec29da552ecf6fb0cccc84e04cab7a1a9b47ad
chris houghton <cehoughton71@gmail.com>
2015-10-18 11:35:41 -0700

    add some bootstrap style

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
 $(document).ready(function() {
-  $('#blanks form').submit(function(event) {
-    var numberInput = $("input#number").val();
-    for (var index = 1; index <= number; index += 1)
+  //$('#blanks form').submit(function(event) {
+    //var numberInput = $("input#number").val();
+var number = parseInt(prompt("Ping Pong?"));
+    for (var index = 1; index <= number; index += 1) {
+      if (index % 15 === 0) {
+        $('#list').append('<li>' + 'Ping-Pong' + '</li>');
+      } else if (index % 3 === 0) {
+          $('#list').append('<li>' + 'Ping' + '</li>');
+      } else if (index % 5 === 0) {
+          $('#list').append('<li>' + 'Pong' + '</li>');
+      } else {
+          $('#list').append('<li>' + index + '</li>');
+      }
+    };
     
-    $('.number').text(numberInput);
+    //$('.number').text(numberInput);
    
 
-    $('#list').show();
+    //$('#list').show();
     
-    event.preventDefault();
+    //event.preventDefault();
   });
-});
+//});
